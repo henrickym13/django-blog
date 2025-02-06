bbb4909c39fef1526fb991f2f7deea84f6ede75c
unknown <henrique.so.ad@hotmail.com>
2025-02-05 21:12:14 -0300

    hotfix

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,9 +104,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR =[
-    os.path.join(BASE_DIR, 'app/static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "app/static",
 ]
 
 # Default primary key field type
