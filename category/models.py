00e99184e6a93e614b6019625da9a3f55db71658
unknown <henrique.so.ad@hotmail.com>
2025-02-07 19:25:50 -0300

    app category

---
+++ category/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
