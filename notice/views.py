1fc9cf1ad0fe8ee5323ea3fffc237800f5ebc175
unknown <henrique.so.ad@hotmail.com>
2025-02-19 16:24:53 -0300

    pagination

--- notice/views.py
+++ notice/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Notice
 from category.models import Category
 
 # Create your views here.
 def notice_list(request):
     first_notice = Notice.objects.first()
-    notices = Notice.objects.all()[1:]
+    notice_list = Notice.objects.all()[1:]
+
+    paginator = Paginator(notice_list, 4)
+    page_number = request.GET.get('page')
+    notices = paginator.get_page(page_number)
 
     return render(request, 'notice_list.html', {'notices': notices, 'first_notice': first_notice})
 
@@ -18,9 +23,13 @@ def show_notice(request, id):
 
 def show_notice_for_category(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    notices = Notice.objects.filter(category=category)
+    notice_list = Notice.objects.filter(category=category)
 
-    first_notice = notices.first()
-    notices = notices[1:]
+    first_notice = notice_list.first()
+    notices = notice_list[1:]
+
+    paginator = Paginator(notices, 4)
+    page_number = request.GET.get('page')
+    notices = paginator.get_page(page_number)
 
     return render(request, 'notice_list.html', {'notices': notices, 'first_notice': first_notice})
